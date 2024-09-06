c47f203e0254d47bb8821b22515106e56a02d406
5AMsan <s.petat@sporting-comm.fr>
2024-09-06 17:25:19 +0200

    Added Vimeo player args

--- includes/PepiteSlider.php
+++ includes/PepiteSlider.php
@@ -78,52 +78,48 @@ class PESL_PepiteSliderModule extends DiviExtension
 
 		});
 
-		// add_filter('oembed_fetch_url',[ $this, 'pesl_oembed_args'], 10, 3);		
 		add_filter('oembed_result', function ($return, $url, $v) {
-			if (strstr($return, 'youtube.com/embed/')) {
+			if (strstr($return, 'youtube.com')) {
 				$id_and_args = explode('watch?v=', $url);
 				$id = explode("&", $id_and_args[1]);
 
 				// edit url to add options
-				$return = str_replace("?feature=oembed", "?feature=oembed&autoplay=1&controls=0&enablejsapi=1&loop=1&modestbrandi=1&mute=1&rel=0&showinfo=0", $return);
+				$return = str_replace("?feature=oembed", "?feature=oembed&controls=0&enablejsapi=1&loop=1&modestbranding=1&mute=1&rel=0&showinfo=0", $return);
 				$add_id = str_replace( 'allowfullscreen>', 'allowfullscreen id="yt-'.$id[0].'">', $return );
 				return $add_id;
 			}
-			else if (strstr($return, 'vimeo.com/video/')) {
+			else if (strstr($return, 'vimeo.com')) {
 				// edit url to add options
-				return str_replace( $url, "$url&autoplay=1", $return);
+				return str_replace( $url, "$url&controls=false&loop=true&pip=false&autoplay=true&autopause=true", $return);
 			}
 		}, 10, 3);
 
+		/**
+		 * Add parameters to embed
+		 * @src https://foxland.fi/adding-parameters-to-wordpress-oembed/
+		 * @src https://github.com/WordPress/WordPress/blob/ec417a34a7ce0d10a998b7eb6d50d7ba6291d94d/wp-includes/class-oembed.php#L553
+		 */
+		add_filter('oembed_fetch_url', function ($provider, $url, $args) {
+			
+			if( stripos($provider, "https://www.youtube.com/") !== false ) {
+				$provider = add_query_arg("modestbranding", 1, $provider);
+				$provider = add_query_arg("controls", 0, $provider);
+				$provider = add_query_arg("rel", 0, $provider);
+			}
+
+			if( stripos($provider, "https://vimeo.com/") !== false ) {
+				$provider = add_query_arg("controls", "false", $provider);
+				$provider = add_query_arg("loop", "true", $provider);
+				$provider = add_query_arg("pip", "false", $provider);
+				$provider = add_query_arg("autopause", "true", $provider);
+				$provider = add_query_arg("autoplay", "true", $provider);
+				// $provider = add_query_arg("background", true, $provider);
+			}
+
+			return $provider;
+		}, 10, 3);
+
 	}
-
-	/**
-	 * Add parameters to embed
-	 * @src https://foxland.fi/adding-parameters-to-wordpress-oembed/
-	 * @src https://github.com/WordPress/WordPress/blob/ec417a34a7ce0d10a998b7eb6d50d7ba6291d94d/wp-includes/class-oembed.php#L553
-	 */
-
-
-	function pesl_oembed_args($provider, $url, $args) {
-
-		$allowed_args = ['autoplay', 'controls', 'modestbranding', 'mute'];
-		
-		$filtered_args = array_filter(
-			$args,
-			function ($key) use ($allowed_args) {
-				return !in_array($key, $allowed_args);
-			},
-			ARRAY_FILTER_USE_KEY
-		);
-		// var_dump( $filtered_args );
-		foreach ($filtered_args as $key => $value) {
-			$provider = add_query_arg($key, $value, $provider);
-		}
-		// $provider = add_query_arg("autoplay", 1, $provider);
-
-		return $provider;
-	}
-
 
 }
 
--- scripts/frontend.js
+++ scripts/frontend.js
@@ -1,46 +1,48 @@
-
+// window.swiperVideoInit = function (data) {
 window.onSwiperTransitionEnd = function (data) {
     let i = $(".swiper-slide").length;
+    console.log( i )
     for ( var j = 0; j < i; j++) {
         try {
             // Get jQuery iFrame
             const iframe = $(data.el).find('.pesl_pepite_slider_item').eq(j).find('iframe').eq(0)
-            
 
             // Youtube
-            if( iframe.attr('src').indexOf('youtube') !== -1 ) {
+            if( iframe.attr('src').indexOf('youtube.com') !== -1 ) {
 
                 // Test HtmlElement prop
                 if ( iframe.get(0).contentWindow) {
                     // Play incoming slide
                     if ( j === data.activeIndex ) {
-                        // console.log("Play " + player.attr('id') )
                         iframe.get(0).contentWindow.postMessage('{\"event\":\"command\",\"func\":\"playVideo\",\"args\":\"\"}', '*');
                     } 
                     // Pause all other slides
                     else {
-                        // console.log("Pause " + player.attr('id') )
                         iframe.get(0).contentWindow.postMessage('{\"event\":\"command\",\"func\":\"pauseVideo\",\"args\":\"\"}', '*');
                     }
                 }
             }
             
             // Vimeo
-            else if (  iframe.attr('src').indexOf('vimeo') !== -1 ) {
+            else if (  iframe.attr('src').indexOf('vimeo.com') !== -1 ) {
                 const vimeoPlayer = new Vimeo.Player(iframe);
+                // console.log( vimeoPlayer)
                 // Play incoming slide
                 if ( j === data.activeIndex ) {
-                    vimeoPlayer.play();
+                    vimeoPlayer.play().then(()=>{console.log("play")});
                 }
                 // Pause all other slides
                 else {
-                    // console.log("Pause " + player.attr('id') )
-                    vimeoPlayer.pause();
+                    vimeoPlayer.pause().then(()=>{console.log("pause")});
                 }
+                // console.log(j, data.activeIndex)
             }
+
         }
         catch(e){
+            console.debug( e )
             continue
         }
     }
 }
+// window.onSwiperTransitionEnd = swiperVideoInit;
